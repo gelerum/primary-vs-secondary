eef3b78031e7a8447df13b2a7d8b6544b04dc669
Gleb Gonchar <gelerum@gmail.com>
2026-02-26 19:38:24 +0700

    add discounting, knn mean price to the pipeline

--- src/normalization.py
+++ src/normalization.py
@@ -52,3 +52,20 @@ def normalize_datasets(dfs):
             df["date"] = df["date"].dt.normalize()
 
     return dfs
+
+
+def normalize_price(df):
+    df["year_month"] = df["date"].dt.to_period("M")
+
+    monthly_stats = df.groupby("year_month", as_index=False).agg(
+        mean_price=("price", "mean")
+    )
+    base_value = monthly_stats["mean_price"].iloc[-1]
+    monthly_stats["index"] = monthly_stats["mean_price"] / base_value
+
+    df = df.merge(monthly_stats[["year_month", "index"]], on="year_month", how="left")
+
+    df["price_normalized"] = df["price"] / df["index"]
+    df["price_per_square_meter_normalized"] = df["price_normalized"] / df["area"]
+
+    return df
--- src/process_data.py
+++ src/process_data.py
@@ -1,23 +1,26 @@
 import pandas as pd
-from src.data_loader import read_dfs
+
 from src.adapters import (
-    adapt_dataframes,
     DF1_ADAPTER,
     DF2_ADAPTER,
     DF3_ADAPTER,
     DF4_ADAPTER,
+    adapt_dataframes,
 )
-from src.normalization import normalize_datasets
+from src.data_loader import read_dfs
+from src.normalization import normalize_price
 from src.filters import (
     drop_nan_addresses,
     drop_nan_prices,
-    filter_by_geo,
     filter_by_area,
     filter_by_floor,
+    filter_by_geo,
     filter_by_price,
 )
 from src.geocoding import geocode_addresses
+from src.normalization import normalize_datasets
 from src.type_casting import cast_types
+from src.feautures import compute_mean_knn_by_geo, compute_ratio_knn
 
 
 def main():
@@ -55,12 +58,39 @@ def main():
 
     df_filter_by_price = filter_by_price(df_filter_by_floor, 1_100_000, 7_300_000_000)
 
-    df_final = df_filter_by_price
+    df_price_normalized = normalize_price(df_filter_by_price)
+
+    df_primary = df_price_normalized[
+        df_price_normalized["market_type"] == "primary"
+    ].copy()
+
+    df_secondary = df_price_normalized[
+        df_price_normalized["market_type"] == "secondary"
+    ].copy()
+
+    df_primary, coords_primary, _ = compute_mean_knn_by_geo(df_primary, k=30, h=500)
+    df_secondary, _, tree_secondary = compute_mean_knn_by_geo(df_secondary, k=30, h=500)
+
+    df_primary = compute_ratio_knn(
+        df_primary,
+        df_secondary,
+        coords_primary,
+        tree_secondary,
+        k=30,
+    )
+
+    df_mean_knn = pd.concat([df_primary, df_secondary])
+    df_mean_knn = df_mean_knn.sort_index()
+
+    df_final = df_mean_knn
+
+    path_to_save = "data/processed/v3/"
 
     df_final.to_parquet(
-        "data/processed/v2/housing_residential_processed.parquet", index=False
+        path_to_save + "housing_residential_processed.parquet", index=False
     )
-    df_final.to_csv("data/processed/v2/housing_residential_processed.csv", index=False)
+    df_final.to_csv(path_to_save + "housing_residential_processed.csv", index=False)
+
     print(f"Final dataset: {df_final.shape[0]} rows x {df_final.shape[1]} columns")
 
 
