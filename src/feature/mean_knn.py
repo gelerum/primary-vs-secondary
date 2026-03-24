2f663080edbeaafc19c5720f3c9a57d58d30960e
Дьячук Савелий <112943229+Sawel54@users.noreply.github.com>
2026-03-24 15:56:43 +0700

    knn modified

--- src/feature/mean_knn.py
+++ src/feature/mean_knn.py
@@ -8,26 +8,28 @@ def compute_mean_knn_by_geo(df, k: int, h: float):
 
     k += 1  # берем на одну больше, так как саму точку не учитываем
 
-    # 2. строим дерево
-
     dist, ind = tree.query(coords, k=k)
     dist_m = dist * 6371000
 
-    prices = df["price"].values
+    prices = df["price_normalized"].values
+    prices_per_m2 = df["price_per_square_meter_normalized"].values
 
-    # удаляем первый столбец (сама точка)
+    # удаляем первый самую точку
     dist_m = dist_m[:, 1:]
     ind = ind[:, 1:]
+
+    max_dist_knn = np.max(dist_m)
+    df["max_distance_knn"] = max_dist_knn
 
     dist_m[dist_m == 0] = 1
 
     weights = np.exp(-dist_m / h)
 
     weighted_price = np.sum(weights * prices[ind], axis=1) / np.sum(weights, axis=1)
+    weighted_price_per_m2 = np.sum(weights * prices_per_m2[ind], axis=1) / np.sum(weights, axis=1)
 
-    df["knn_weighted_price"] = weighted_price
-
-    df["knn_weighted_price_per_square_meter"] = df["knn_weighted_price"] / df["area"]
+    df["knn_weighted_price_normalized"] = weighted_price
+    df["knn_weighted_price_per_square_meter_normalized"] = weighted_price_per_m2
 
     return df, coords, tree
 
@@ -41,10 +43,10 @@ def compute_ratio_knn(
 ):
     dist, ind = tree_secondary.query(coords_primary, k=k)
 
-    secondary_vals = df_secondary["knn_weighted_price"].values[ind]
+    secondary_vals = df_secondary["knn_weighted_price_per_square_meter_normalized"].values[ind]
     secondary_mean = secondary_vals.mean(axis=1)
 
-    primary_vals = df_primary["knn_weighted_price"].values
+    primary_vals = df_primary["knn_weighted_price_per_square_meter_normalized"].values
 
     ratio = primary_vals / secondary_mean
 
