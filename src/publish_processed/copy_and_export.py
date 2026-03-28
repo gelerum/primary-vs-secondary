480abd35b55e87dde06729b5219ceb2c60c1fadc
Gleb Gonchar <gelerum@gmail.com>
2026-03-29 03:24:44 +0700

    fix

---
+++ src/publish_processed/copy_and_export.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_parquet("data/interim/04_mean_knn.parquet")
+
+df.to_parquet("data/processed/final.parquet", index=False)
+df.to_csv("data/processed/final.csv", index=False)
