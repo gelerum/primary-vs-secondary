4dd7bd9e12b7d9d561761af580200cfce5f15f03
Gleb Gonchar <gelerum@gmail.com>
2026-04-08 17:39:43 +0700

    merge wnir with administrative districts

--- src/stages/06_publish_processed.py
+++ src/stages/06_publish_processed.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def main():
-    df = pd.read_parquet("data/interim/04_mean_knn.parquet")
+    df = pd.read_parquet("data/interim/05_administrative_district.parquet")
 
     df.to_parquet("data/processed/final.parquet", index=False)
     df.to_csv("data/processed/final.csv", index=False)
