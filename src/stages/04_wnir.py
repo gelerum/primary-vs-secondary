db506fb247634bf0528c9b19ff0f9206c787426b
Gleb Gonchar <gelerum@gmail.com>
2026-04-19 02:42:21 +0700

    add dataset

--- src/stages/04_wnir.py
+++ src/stages/04_wnir.py
@@ -1,18 +1,23 @@
+from dvc.api import params_show
 import pandas as pd
 
 from src.wnir.wnir import compute_wnir, compute_ratio_wnir
 
-R = 500
-h = 100
-
 
 def main():
+    params = params_show()["02_geocode"]
+
     df = pd.read_parquet("data/interim/03_price_discounted.parquet")
 
     df_primary = df[df["market_type"] == "primary"].copy()
 
     df_secondary = df[df["market_type"] == "secondary"].copy()
 
+    h = params["h"]
+    R = 500
+    # Rs = params["R"].values()
+    # for r in Rs:
+    #     R = 500
     df_primary, tree_primary = compute_wnir(df_primary, R=R, h=h)
     df_secondary, tree_secondary = compute_wnir(df_secondary, R=R, h=h)
 
